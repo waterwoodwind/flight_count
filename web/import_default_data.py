a97d7e685e9845fe8436e50488233b8e531439a1
waterwoodwind <469798015@qq.com>
2017-03-24 16:08:54 +0800

    初始化飞机清单及实际表起点

---
+++ web/import_default_data.py
@@ -0,0 +1,35 @@
+#coding=utf-8
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "flight_count.settings")
+
+
+import django
+django.setup()
+
+from beijing_flight_count.models import *
+
+def import_model(txt_file,model_object):
+    f = open(txt_file)
+    txt = f.read()
+    f.close()
+    objectList = []
+    title = txt.split('，')
+    print title
+    for item in title:
+        single_object = model_object(name=item)
+        objectList.append(single_object)
+    model_object.objects.bulk_create(objectList)
+
+def import_actual_begin():
+    ac_all = Ac_Number.objects.all()
+    for ac in ac_all:
+        loc = Location.objects.get(name=u"重庆")
+        check = Check_Option.objects.get(name=u"是")
+        ActualTimetable.objects.create(actual_ac_number=ac,
+                                       actual_date="2017-03-24",
+                                       actual_location=loc,
+                                       actual_scheduled_check=check)
+
+if __name__ == "__main__":
+    import_actual_begin()
+    print('Done!')
